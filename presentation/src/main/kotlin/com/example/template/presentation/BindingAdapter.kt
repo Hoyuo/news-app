0602cff8c0f71bc4553f8da8b4cf5f3439a9fce1
hoyuo <psh9907@gmail.com>
2023-12-11 18:06:19 +0900

    Update DiskCache

--- presentation/src/main/kotlin/com/example/template/presentation/BindingAdapter.kt
+++ presentation/src/main/kotlin/com/example/template/presentation/BindingAdapter.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.template.presentation.main.adapter.HeadlineAdapter
 import com.example.template.presentation.model.ArticleUiModel
 
@@ -14,9 +15,13 @@ fun RecyclerView.bindList(list: List<ArticleUiModel>?) {
 
 @BindingAdapter("bind:imageUrl")
 fun ImageView.bindImageUrl(url: String?) {
-    url?.let {
+    if (url.isNullOrEmpty()) {
         Glide.with(this)
-            .load(it)
+            .clear(this)
+    } else {
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(this)
     }
 }
